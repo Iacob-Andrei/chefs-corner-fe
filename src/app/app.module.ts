747156c5b81509f646cca1c54c2ab3d27bb0f753
Andrei Iacob <andreiiacob15@gmail.com>
2023-02-01 13:48:26 +0200

    topbar added + angular materials + flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { TopBarComponent } from './authentification/top-bar/top-bar.component';
+import { LoginFormComponent } from './authentification/login-form/login-form.component';
+import { RegisterFormComponent } from './authentification/register-form/register-form.component';
+import { AuthentificationContainer } from './authentification/container/authentification-container.component';
+import {FlexModule} from "@angular/flex-layout";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    TopBarComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    AuthentificationContainer,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AuthentificationContainer]
 })
 export class AppModule { }
---
+++ src/app/authentification/container/authentification-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './authentification-container.component.html',
+  styleUrls: ['./authentification-container.component.scss']
+})
+export class AuthentificationContainer {
+
+}
