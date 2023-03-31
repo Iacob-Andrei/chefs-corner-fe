843ed07d033d0b6d07e4afd9101958e73ceda303
aiacob <andreiiacob15@gmail.com>
2023-03-31 19:47:37 +0300

    price with profit

--- src/app/modules/recipes/components/price-dialog/price-dialog.component.ts
+++ src/app/modules/recipes/components/price-dialog/price-dialog.component.ts
@@ -7,22 +7,38 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 })
 export class PriceDialogComponent implements OnInit{
   ingredients!: any[];
-  quantities!: any[];
+  quantities!: any;
   totalPrice = 0;
+  extra = 0;
+  tax =  15;
+  percentages = [
+    {"procent": 10, "view": "10%"},
+    {"procent": 15, "view": "15%"},
+    {"procent": 20, "view": "20%"},
+    {"procent": 25, "view": "25%"},
+    {"procent": 30, "view": "30%"},
+  ]
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
     this.ingredients = this.data.ingredients;
-    this.quantities = this.data.quantities
+    this.quantities = this.data.quantities;
 
-    this.ingredients.forEach(item => this.totalPrice += item.amount * item.price_per_unit)
-    this.totalPrice = Math.round(this.totalPrice * 100) / 100
-
-    console.log(this.ingredients)
-    console.log(this.quantities)
+    this.ingredients.forEach(item => this.totalPrice += this.getIngredientActualAmount(item.id) * item.price_per_unit);
+    this.totalPrice = Math.round(this.totalPrice * 100) / 100;
+    this.extra = Math.round(this.totalPrice * (this.tax + 100) ) / 100;
   }
 
   getProduct(value1: number, value2: number): number{
     return Math.round(value1 * value2 * 100) / 100
+  }
+
+  getIngredientActualAmount(id: string): number{
+    const key = id + "_amount"
+    return this.quantities[key].value;
+  }
+
+  changePercent() {
+    this.extra = Math.round(this.totalPrice * (this.tax + 100) ) / 100;
   }
 }
