3b65324c4904809383e5deccde5a7891d96a97df
aiacob <andreiiacob15@gmail.com>
2023-04-12 22:00:13 +0300

    directions added

--- src/app/modules/recipes/components/create-recipe/create-recipe.component.ts
+++ src/app/modules/recipes/components/create-recipe/create-recipe.component.ts
@@ -5,7 +5,7 @@ import {
 import {map, Observable} from "rxjs";
 import {StepperOrientation} from "@angular/cdk/stepper";
 import {BreakpointObserver} from "@angular/cdk/layout";
-import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {MatStepper} from "@angular/material/stepper";
 
 @Component({
@@ -32,9 +32,14 @@ export class CreateRecipeComponent{
   secondFormGroup = this._formBuilder.record({
     ingredient1: new FormControl('', Validators.required),
     description1: new FormControl('', Validators.required),
-    units1: new FormControl(0, [Validators.required, Validators.min(0)]),
-    gramsPerUnit1: new FormControl(0, [Validators.required, Validators.min(0)]),
+    units1: new FormControl(1, [Validators.required, Validators.min(1)]),
+    gramsPerUnit1: new FormControl(1, [Validators.required, Validators.min(1)]),
   });
+
+  directionsCount: number[] = [1];
+  thirdFormGroup = this._formBuilder.record({
+    direction1: new FormControl('', Validators.required)
+  })
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -47,7 +52,26 @@ export class CreateRecipeComponent{
   }
 
   onConfirmClick() {
+    console.log("DETAILS:")
+    console.log(this.firstFormGroup.controls.title.value)
+    console.log(this.firstFormGroup.controls.cookTime.value)
+    console.log(this.firstFormGroup.controls.prepTime.value)
+    console.log(this.recipeImageName)
 
+    console.log("INGREDIENTS:")
+    this.ingredientsCount.forEach(index => {
+      console.log(
+        this.secondFormGroup.get(`ingredient${index}`)?.value,
+        this.secondFormGroup.get(`description${index}`)?.value,
+        this.secondFormGroup.get(`units${index}`)?.value,
+        this.secondFormGroup.get(`gramsPerUnit${index}`)?.value
+      )
+    })
+
+    console.log("INSTRUCTIONS:")
+    this.directionsCount.forEach(index => {
+      console.log(this.thirdFormGroup.get(`direction${index}`)?.value)
+    })
   }
 
   onFileSelected(event: any) {
@@ -58,9 +82,9 @@ export class CreateRecipeComponent{
   }
 
   toSecondStepper() {
-    // if (this.recipeImageName !== '') {
+    if (this.recipeImageName !== '') {
       this.stepper.next();
-    // }
+    }
   }
 
   addIngredient() {
@@ -72,16 +96,40 @@ export class CreateRecipeComponent{
       ...this.secondFormGroup.controls,
       [`ingredient${last+1}`]: new FormControl('', Validators.required),
       [`description${last+1}`]: new FormControl('', Validators.required),
-      [`units${last+1}`]: new FormControl(0, [Validators.required, Validators.min(0)]),
-      [`gramsPerUnit${last+1}`]: new FormControl(0, [Validators.required, Validators.min(0)]),
+      [`units${last+1}`]: new FormControl(1, [Validators.required, Validators.min(1)]),
+      [`gramsPerUnit${last+1}`]: new FormControl(1, [Validators.required, Validators.min(1)]),
     });
+  }
 
-    Object.keys(this.secondFormGroup.controls).forEach((key: string) => {
-      console.log(key, this.secondFormGroup.get(key)?.value)
+  addDirection(){
+    let last = this.directionsCount.pop();
+    last = last ? last : 1;
+    this.directionsCount.push(last, last + 1);
+
+    this.thirdFormGroup = this._formBuilder.record({
+      ...this.thirdFormGroup.controls,
+      [`direction${last+1}`]: new FormControl('', Validators.required)
     })
   }
 
-  getFormControl(name: string, index: number) {
-    return this.secondFormGroup.get(name + index) as FormControl<string | number | null>;
+  getFormControl(name: string, index: number, form: FormGroup) {
+    return form.get(name + index) as FormControl<string | number | null>;
+  }
+
+  removeIngredient(index: number) {
+    this.secondFormGroup.removeControl(`ingredient${index}`);
+    this.secondFormGroup.removeControl(`description${index}`);
+    this.secondFormGroup.removeControl(`units${index}`);
+    this.secondFormGroup.removeControl(`gramsPerUnit${index}`);
+    this.ingredientsCount = this.ingredientsCount.filter(obj => {
+      return obj !== index
+    });
+  }
+
+  removeDirection(index: number) {
+    this.thirdFormGroup.removeControl(`direction${index}`);
+    this.directionsCount = this.directionsCount.filter(obj => {
+      return obj !== index
+    });
   }
 }
